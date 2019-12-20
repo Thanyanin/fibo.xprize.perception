aa01243c08d82e8ef0a7c4182e0c34cce913cc3e
kong4580 <34432650+kong4580@users.noreply.github.com>
2019-12-20 15:03:27 +0700

    รวม streamภาพและ การหมุนของหุ่นด้วยกัน
    ยังมีปัญหามุมหมุนเกินอยู่

--- Vision.Operator/source/MainVision/Assets/mainscript/mqtest.cs
+++ Vision.Operator/source/MainVision/Assets/mainscript/mqtest.cs
@@ -11,30 +11,33 @@ using System;
 public class mqtest : MonoBehaviour
 {
     public GameObject vrcam;
+    public GameObject Head;
+    public GameObject body;
+    public GameObject shoulderLeft;
+    public GameObject shoulderRight;
     private MqttClient client;
     float init_roll = 0;
     float init_pitch = 0;
     float init_yaw = 0;
     int state_start = 0;
+    int set_zero = 0;
     float cur_time = 0;
+
     // Use this for initialization
     void Start()
     {
         // create client instance 
-        client = new MqttClient(IPAddress.Parse("192.168.2.246"), 1883, false, null);
+        client = new MqttClient(IPAddress.Parse("192.168.1.102"), 1883, false, null);
 
-        // register to message received 
+        // register to message received
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
         string clientId = Guid.NewGuid().ToString();
         client.Connect(clientId);
 
-        // subscribe to the topic "/home/temperature" with QoS 2 
+        // subscribe to the topic "/home/temperature" with QoS 2
         client.Subscribe(new string[] { "hello/world" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-        //var init_ang = vrcam.transform.rotation.eulerAngles;
-        //init_roll = init_ang[2];
-        //init_pitch = init_ang[0];
-        //init_yaw = init_ang[1];
+
         StartCoroutine(Example());
 
     }
@@ -43,70 +46,127 @@ public class mqtest : MonoBehaviour
 
         Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
     }
-    float update_value(float start, float current)
+    string trackWithRef(Quaternion refer, Quaternion track)
     {
-        current = current - start;
-        if (current < 0)
-        {
-            current = 360 + current;
-        }
-        if (current >= 180)
-        {
-            current = current - 360;
-        }
-        return current;
+        var x_r = refer[0];
+        var y_r = refer[1];
+        var z_r = refer[2];
+        var w_r = refer[3];
+
+        var x_t = track[0];
+        var y_t = track[1];
+        var z_t = track[2];
+        var w_t = track[3];
+
+        var w = (w_r * w_t) - (x_r * x_t) - (y_r * y_t) - (z_r * z_t);
+        var x = (w_r * x_t) - (w_t * x_r) - (y_r * z_t) + (z_r * y_t);
+        var y = (w_r * y_t) - (w_t * y_r) - (x_r * z_t) + (z_r * x_t);
+        var z = (w_r * z_t) - (w_t * z_r) - (x_r * y_t) + (y_r * x_t);
+
+        //if(set_zero == 1)
+        //{
+
+        //}
+
+        return w + "," + x + "," + y + "," + z;
     }
+    //string trackWithRef(Quaternion refer, Quaternion track)
+    //{
+    //    var x_r = refer[0];
+    //    var y_r = refer[1];
+    //    var z_r = refer[2];
+    //    var w_r = refer[3];
+
+    //    var x_t = track[0];
+    //    var y_t = track[1];
+    //    var z_t = track[2];
+    //    var w_t = track[3];
+
+    //    var w = (w_r * w_t) - (x_r * x_t) - (y_r * y_t) - (z_r * z_t);
+    //    var x = (w_r * x_t) - (w_t * x_r) - (y_r * z_t) + (z_r * y_t);
+    //    var y = (w_r * y_t) - (w_t * y_r) - (x_r * z_t) + (z_r * x_t);
+    //    var z = (w_r * z_t) - (w_t * z_r) - (x_r * y_t) + (y_r * x_t);
+
+    //    return w + "," + x + "," + y + "," + z;
+    //}
     void OnGUI()
     {
-        if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
-        {
-            Debug.Log("sending...");
-            var init_ang = vrcam.transform.rotation.eulerAngles;
-            var cur_roll = init_ang[2];
-            var cur_pitch = init_ang[0];
-            var cur_yaw = init_ang[1];
+        //if (GUI.Button(new Rect(20, 40, 80, 20), "Set Home"))
+        //{
+        //    Debug.Log("SetHome");
 
-            init_roll = cur_roll;
-            init_pitch = cur_pitch;
-            init_yaw = cur_yaw;
+
+
+
+        //    //var init_ang = vrcam.transform.rotation.eulerAngles;
+        //    //var cur_roll = init_ang[2];
+        //    //var cur_pitch = init_ang[0];
+        //    //var cur_yaw = init_ang[1];
+
+        //    //init_roll = cur_roll;
+        //    //init_pitch = cur_pitch;
+        //    //init_yaw = cur_yaw;
+        //    set_zero = 1;
+        //    Debug.Log("SetHome Done");
+        //}
+        if (GUI.Button(new Rect(100, 40, 80, 20), "Send DATA"))
+        {
+            Debug.Log("Map Ip 192.168.3.201:1883");
+            Debug.Log("start sending");
             state_start = 1;
             Debug.Log("sent");
+        }
+        if (GUI.Button(new Rect(180, 40, 80, 20), "Stop"))
+        {
+            Debug.Log("stop sending...");
+
+            state_start = 0;
+            Debug.Log("stop");
         }
     }
     IEnumerator Example()
     {
         cur_time = Time.time;
         yield return 0;
-        var pos = vrcam.transform.position;
         var ang = vrcam.transform.rotation.eulerAngles;
-        var cur_roll = ang[2];
-        var cur_pitch = ang[0];
-        var cur_yaw = ang[1];
+        var roll = ang[0];
+        var pitch = ang[1];
+        var yaw = ang[2];
+        var q_vrcam = vrcam.transform.rotation;
+        var q_body = body.transform.rotation;
+        var q_shoulderLeft = shoulderLeft.transform.rotation;
+        var q_shoulderRight = shoulderRight.transform.rotation;
 
-        cur_roll = update_value(init_roll, cur_roll);
-        cur_pitch = update_value(init_pitch, cur_pitch);
-        cur_yaw = update_value(init_yaw, cur_yaw);
-
-
-
-        var roll = cur_roll.ToString();
-        var pitch = cur_pitch.ToString();
-        var yaw = cur_yaw.ToString();
-        var all = roll + "," + pitch + "," + yaw;
+        var bodyWithGlobal = q_body[3] + "," + q_body[0] + "," + q_body[1] + "," + q_body[2];
+        var headWithBody = trackWithRef(q_body, q_vrcam);
+        var sholderLeftWithBody = trackWithRef(q_body, q_shoulderLeft);
+        var sholderRightWithBody = trackWithRef(q_body, q_shoulderRight);
+        var headrotaion = Head.transform.localRotation;
+        string s_headrotation = headrotaion[0] + "," + headrotaion[1] + "," + headrotaion[2] + "," + headrotaion[3];
+        string s_rpy = headrotaion[0] + "," + headrotaion[1] + "," + headrotaion[2] + "," + headrotaion[3];
         //Debug.Log(vrcam.transform.position);
         var topic_position_roll = "/operator/roll";
         var topic_position_pitch = "/operator/pitch";
         var topic_position_yaw = "/operator/yaw";
-        var topic_position_rotation = "/operator/rotation";
+        var topic_position_headrotation = "/operator/headrotation";
+        var topic_position_bodyrotation = "/operator/bodyrotation";
+        var topic_position_shoulderleftrotation = "/operator/shoulderleftrotation";
+        var topic_position_shoulderrightrotation = "/operator/shoulderrightrotation";
+
 
         //Debug.Log(roll);
-        client.Publish(topic_position_roll, System.Text.Encoding.UTF8.GetBytes(roll), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-        client.Publish(topic_position_pitch, System.Text.Encoding.UTF8.GetBytes(pitch), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-        client.Publish(topic_position_yaw, System.Text.Encoding.UTF8.GetBytes(yaw), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        //client.Publish(topic_position_roll, System.Text.Encoding.UTF8.GetBytes(roll), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        //client.Publish(topic_position_pitch, System.Text.Encoding.UTF8.GetBytes(pitch), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        //client.Publish(topic_position_yaw, System.Text.Encoding.UTF8.GetBytes(yaw), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
         if (state_start == 1)
         {
-            Debug.Log(all);
-            client.Publish(topic_position_rotation, System.Text.Encoding.UTF8.GetBytes(all), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            client.Publish(topic_position_headrotation, System.Text.Encoding.UTF8.GetBytes(s_headrotation), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            Debug.Log(headrotaion.eulerAngles);
+            //client.Publish(topic_position_rotation, System.Text.Encoding.UTF8.GetBytes(all), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            //client.Publish(topic_position_headrotation, System.Text.Encoding.UTF8.GetBytes(headWithBody), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            //client.Publish(topic_position_bodyrotation, System.Text.Encoding.UTF8.GetBytes(bodyWithGlobal), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            //client.Publish(topic_position_shoulderleftrotation, System.Text.Encoding.UTF8.GetBytes(sholderLeftWithBody), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            //client.Publish(topic_position_shoulderrightrotation, System.Text.Encoding.UTF8.GetBytes(sholderRightWithBody), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
         }
         
         //Debug.Log("sent");
---
+++ Vision.Operator/source/MainVision/Assets/mainscript/q_translation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class q_translation : MonoBehaviour
+{
+
+    public Transform ObjBodyTarget;
+    public Transform ObjHeadTarget;
+    public Transform ObjHeadPtr;
+    public Transform ObjBodyPtr;
+    public Transform CalibrateObj;
+    public Transform CalibrateObjBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    float temp_r;
+    float temp_p;
+    float temp_y;
+    float temp_h_r;
+    float temp_h_p;
+    float temp_h_y;
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Set Home"))
+        {
+            Debug.Log("SetHome");
+            temp_r = ObjBodyTarget.eulerAngles.x;
+            temp_p = ObjBodyTarget.eulerAngles.y;
+            temp_y = ObjBodyTarget.eulerAngles.z;
+            temp_h_r = ObjHeadTarget.localEulerAngles.x;
+            temp_h_p = ObjHeadTarget.localEulerAngles.y;
+            temp_h_y = ObjHeadTarget.localEulerAngles.z;
+            //CalibrateObj.eulerAngles = new Vector3(ObjBodyPtr.eulerAngles.x- ObjBodyPtr.eulerAngles.x, ObjBodyPtr.eulerAngles.y - ObjBodyPtr.eulerAngles.y, ObjBodyPtr.eulerAngles.y - ObjBodyPtr.eulerAngles.y);
+            //ObjBody.transform.rotation = Quaternion.Euler(0,0,0); 
+            CalibrateObj.localEulerAngles = new Vector3(0, 0, 0);
+            CalibrateObjBody.localPosition = new Vector3(0,0,0);
+            CalibrateObjBody.eulerAngles = new Vector3(ObjBodyTarget.eulerAngles.x-temp_r, ObjBodyTarget.eulerAngles.y - temp_p, ObjBodyTarget.eulerAngles.z - temp_y);
+
+            //var init_ang = vrcam.transform.rotation.eulerAngles;
+            //var cur_roll = init_ang[2];
+            //var cur_pitch = init_ang[0];
+            //var cur_yaw = init_ang[1];
+
+            //init_roll = cur_roll;
+            //init_pitch = cur_pitch;
+            //init_yaw = cur_yaw;
+
+            Debug.Log("SetHome Done");
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CalibrateObjBody.localEulerAngles = new Vector3(ObjBodyTarget.eulerAngles.x - temp_r, ObjBodyTarget.eulerAngles.y - temp_p, ObjBodyTarget.eulerAngles.z - temp_y);
+        //มุมบวกทบเกิน
+        CalibrateObj.localEulerAngles = new Vector3(ObjHeadTarget.eulerAngles.x - temp_h_r, ObjHeadTarget.eulerAngles.y - temp_h_p, ObjHeadTarget.eulerAngles.z - temp_h_y);
+
+        //ObjHeadPtr.rotation = ObjHeadTarget.rotation;
+        //ObjBodyPtr.rotation = ObjBodyTarget.rotation;
+        //Debug.Log("ObjBody=" + ObjBody.rotation);
+        //Debug.Log("ObjHeadPtr=" + ObjHeadPtr.localRotation);
+
+    }
+
+
+}
