114421b7ba44389c7801c87ed202e133bcc36ac3
kong4580 <34432650+kong4580@users.noreply.github.com>
2020-01-20 17:15:40 +0700

    update rotation

--- Vision.Operator/source/MainVision/Assets/mainscript/mqtest.cs
+++ Vision.Operator/source/MainVision/Assets/mainscript/mqtest.cs
@@ -15,6 +15,7 @@ public class mqtest : MonoBehaviour
     public GameObject Body;
     public GameObject shoulderLeft;
     public GameObject shoulderRight;
+    public string server_ip = "192.168.2.102";
     private MqttClient client;
     float init_roll = 0;
     float init_pitch = 0;
@@ -22,7 +23,7 @@ public class mqtest : MonoBehaviour
     int state_start = 0;
     int set_zero = 0;
     float cur_time = 0;
-    string server_ip = "192.168.1.102";
+    
     // Use this for initialization
     void Start()
     {
@@ -62,69 +63,9 @@ public class mqtest : MonoBehaviour
         Debug.Log(obj.name + ": " + packed_msg);
         return packed_msg;
     }
-    string trackWithRef(Quaternion refer, Quaternion track)
-    {
-        var x_r = refer[0];
-        var y_r = refer[1];
-        var z_r = refer[2];
-        var w_r = refer[3];
-
-        var x_t = track[0];
-        var y_t = track[1];
-        var z_t = track[2];
-        var w_t = track[3];
-
-        var w = (w_r * w_t) - (x_r * x_t) - (y_r * y_t) - (z_r * z_t);
-        var x = (w_r * x_t) - (w_t * x_r) - (y_r * z_t) + (z_r * y_t);
-        var y = (w_r * y_t) - (w_t * y_r) - (x_r * z_t) + (z_r * x_t);
-        var z = (w_r * z_t) - (w_t * z_r) - (x_r * y_t) + (y_r * x_t);
-
-        //if(set_zero == 1)
-        //{
-
-        //}
-
-        return w + "," + x + "," + y + "," + z;
-    }
-    //string trackWithRef(Quaternion refer, Quaternion track)
-    //{
-    //    var x_r = refer[0];
-    //    var y_r = refer[1];
-    //    var z_r = refer[2];
-    //    var w_r = refer[3];
-
-    //    var x_t = track[0];
-    //    var y_t = track[1];
-    //    var z_t = track[2];
-    //    var w_t = track[3];
-
-    //    var w = (w_r * w_t) - (x_r * x_t) - (y_r * y_t) - (z_r * z_t);
-    //    var x = (w_r * x_t) - (w_t * x_r) - (y_r * z_t) + (z_r * y_t);
-    //    var y = (w_r * y_t) - (w_t * y_r) - (x_r * z_t) + (z_r * x_t);
-    //    var z = (w_r * z_t) - (w_t * z_r) - (x_r * y_t) + (y_r * x_t);
-
-    //    return w + "," + x + "," + y + "," + z;
-    //}
+   
     void OnGUI()
     {
-        //if (GUI.Button(new Rect(20, 40, 80, 20), "Set Home"))
-        //{
-        //    Debug.Log("SetHome");
-
-
-
-
-        //    //var init_ang = vrcam.transform.rotation.eulerAngles;
-        //    //var cur_roll = init_ang[2];
-        //    //var cur_pitch = init_ang[0];
-        //    //var cur_yaw = init_ang[1];
-
-        //    //init_roll = cur_roll;
-        //    //init_pitch = cur_pitch;
-        //    //init_yaw = cur_yaw;
-        //    set_zero = 1;
-        //    Debug.Log("SetHome Done");
-        //}
         if (GUI.Button(new Rect(100, 40, 80, 20), "Send DATA"))
         {
             Debug.Log("Map Ip "+ server_ip);
@@ -144,26 +85,14 @@ public class mqtest : MonoBehaviour
     {
         cur_time = Time.time;
         yield return 0;
-        //var ang = vrcam.transform.rotation.eulerAngles;
-        //var roll = ang[0];
-        //var pitch = ang[1];
-        //var yaw = ang[2];
-        //var q_vrcam = vrcam.transform.localRotation;
-        //var q_body = body.transform.localRotation;
-        //var q_shoulderLeft = shoulderLeft.transform.rotation;
-        //var q_shoulderRight = shoulderRight.transform.rotation;
-
-        //var bodyWithGlobal = q_body[3] + "," + q_body[0] + "," + q_body[1] + "," + q_body[2];
-        //var headWithBody = trackWithRef(q_body, q_vrcam);
-        //var sholderLeftWithBody = trackWithRef(q_body, q_shoulderLeft);
-        //var sholderRightWithBody = trackWithRef(q_body, q_shoulderRight);
+        
         var headrotaion = Head.transform.localRotation;
         var bodyrotaion = Body.transform.localRotation;
         string s_headrotation = headrotaion.x + "," + headrotaion.y + "," + headrotaion.z + "," + headrotaion.w;
         string s_bodyrotation = bodyrotaion.x + "," + bodyrotaion.y + "," + bodyrotaion.z + "," + bodyrotaion.w;
 
         
-        //Debug.Log(vrcam.transform.position);
+        
         var topic_position_roll = "/operator/roll";
         var topic_position_pitch = "/operator/pitch";
         var topic_position_yaw = "/operator/yaw";
@@ -173,10 +102,6 @@ public class mqtest : MonoBehaviour
         var topic_position_shoulderrightrotation = "/operator/shoulderright/rotation";
 
 
-        //Debug.Log(roll);
-        //client.Publish(topic_position_roll, System.Text.Encoding.UTF8.GetBytes(roll), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-        //client.Publish(topic_position_pitch, System.Text.Encoding.UTF8.GetBytes(pitch), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-        //client.Publish(topic_position_yaw, System.Text.Encoding.UTF8.GetBytes(yaw), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
         if (state_start == 1)
         {
             //client.Publish(topic_position_headrotation, System.Text.Encoding.UTF8.GetBytes(s_headrotation), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
--- Vision.Operator/source/MainVision/Assets/mainscript/q_translation.cs
+++ Vision.Operator/source/MainVision/Assets/mainscript/q_translation.cs
@@ -29,7 +29,12 @@ public class q_translation : MonoBehaviour
     Vector3 temp_hand_left_translation;
     Vector3 temp_hand_right_rotation;
     Vector3 temp_hand_right_translation;
-
+    Vector3 head_offset;
+    Vector3 body_offset;
+    Vector3 sholder_left_offset;
+    Vector3 sholder_right_offset;
+    Vector3 hand_left_offset;
+    Vector3 hand_right_offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -43,20 +48,27 @@ public class q_translation : MonoBehaviour
         temp_hand_left_translation = new Vector3(0, 0, 0);
         temp_hand_right_rotation = new Vector3(0, 0, 0);
         temp_hand_right_translation = new Vector3(0, 0, 0);
+
+        head_offset = CalibrateHead.localRotation.eulerAngles;
+        body_offset = CalibrateBody.localRotation.eulerAngles;
+
+        sholder_left_offset = CalibrateSholderLeft.localRotation.eulerAngles;
+        sholder_right_offset = CalibrateSholderRight.localRotation.eulerAngles;
+
     }
-    
+
     void OnGUI()
     {
         if (GUI.Button(new Rect(20, 40, 80, 20), "Set Home"))
         {
             
             Debug.Log("SetHome..");
-            temp_body = DeviceBody.eulerAngles;
-            temp_head = DeviceHead.eulerAngles;
-            temp_hand_right_rotation = DeviceHandRight.eulerAngles;
-            temp_hand_left_rotation = DeviceHandLeft.eulerAngles;
-            temp_sholder_right_rotation = DeviceSholderRight.eulerAngles;
-            temp_sholder_left_rotation = DeviceSholderLeft.eulerAngles;
+            temp_body = DeviceBody.localRotation.eulerAngles;
+            temp_head = DeviceHead.localRotation.eulerAngles;
+            temp_hand_right_rotation = DeviceHandRight.localRotation.eulerAngles;
+            temp_hand_left_rotation = DeviceHandLeft.localRotation.eulerAngles;
+            temp_sholder_right_rotation = DeviceSholderRight.localRotation.eulerAngles;
+            temp_sholder_left_rotation = DeviceSholderLeft.localRotation.eulerAngles;
             temp_hand_right_translation = DeviceHandRight.position;
             temp_hand_left_translation = DeviceHandLeft.position;
             temp_sholder_right_translation = DeviceSholderRight.position;
@@ -66,13 +78,13 @@ public class q_translation : MonoBehaviour
         }
         
     }
-    void calibrate(Transform Device, Transform Calibrate, Vector3 Temp,bool mode)
+    void calibrate(Transform Device, Transform Calibrate, Vector3 Temp,Vector3 offset,bool mode)
     {
         if (mode)
         {
-            Calibrate.eulerAngles = new Vector3(Device.eulerAngles.x - Temp.x,
-                                                Device.eulerAngles.y - Temp.y,
-                                                Device.eulerAngles.z - Temp.z);
+            Calibrate.eulerAngles = new Vector3(Device.localRotation.eulerAngles.x - Temp.x + offset.x,
+                                                Device.localRotation.eulerAngles.y - Temp.y + offset.y,
+                                                Device.localRotation.eulerAngles.z - Temp.z + offset.z);
         }
         else
         {
@@ -85,12 +97,14 @@ public class q_translation : MonoBehaviour
     void Update()
     {
         //Debug.Log("-------------");
-        //Debug.Log(CalibrateBody.localEulerAngles);
-        //Debug.Log(CalibrateHead.localEulerAngles);
-        calibrate(DeviceBody, CalibrateBody, temp_body, true);
-        calibrate(DeviceHead, CalibrateHead, temp_head, true);
-        calibrate(DeviceSholderLeft, CalibrateSholderLeft, temp_sholder_left_rotation, true);
-        calibrate(DeviceSholderLeft, CalibrateSholderLeft, temp_sholder_left_translation, false);
+        Debug.Log(CalibrateBody.localEulerAngles);
+        Debug.Log(CalibrateHead.localEulerAngles);
+        Debug.Log(CalibrateSholderLeft.localEulerAngles);
+
+        calibrate(DeviceBody, CalibrateBody, temp_body,new Vector3(0,0,0), true);
+        calibrate(DeviceHead, CalibrateHead, temp_head, new Vector3(0, 0, 0), true);
+        calibrate(DeviceSholderLeft, CalibrateSholderLeft, temp_sholder_left_rotation, new Vector3(0, -90, 0), true);
+        //calibrate(DeviceSholderLeft, CalibrateSholderLeft, temp_sholder_left_translation, false);
 
     }
 
